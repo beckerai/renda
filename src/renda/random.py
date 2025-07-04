b3bebb1ceee8d1617177f87ba606c3dc8d35edba
Martin Becker <mbecker6@uni-muenster.de>
2025-07-04 13:06:01 +0200

    feat: add renda.random.temp_seed function

--- src/renda/random.py
+++ src/renda/random.py
@@ -11,7 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any
+import random
+from typing import Any, Optional
+
+import numpy as np
+import torch
 
 
 MIN_SEED = 0
@@ -32,3 +36,41 @@ def ensure_seed(value: int) -> int:
     # This only works because MIN_SEED = 0 and MIN_SEED > 0
     # A more general solutions would be nice
     return value % (MAX_SEED + 1)
+
+
+class temp_seed:
+    def __init__(self, seed: Optional[int]) -> None:
+        if not is_seed(seed) and seed is not None:
+            raise ValueError(
+                f"`seed` must be an `int` between `MIN_SEED = {MIN_SEED}` and "
+                f"`MAX_SEED = {MAX_SEED}` or None."
+            )
+
+        self._seed = seed
+
+        self._random_state = None
+        self._np_random_state = None
+        self._torch_rng_state = None
+        self._torch_cuda_rng_state_all = None
+
+    def __enter__(self) -> None:  # pragma: no cover
+        if self._seed is not None:
+            # Store random states
+            self._random_state = random.getstate()
+            self._np_random_state = np.random.get_state()
+            self._torch_rng_state = torch.get_rng_state()
+            self._torch_cuda_rng_state_all = torch.cuda.get_rng_state_all()
+
+            # Seed everything
+            random.seed(self._seed)
+            np.random.seed(self._seed)
+            torch.manual_seed(self._seed)
+            torch.cuda.manual_seed_all(self._seed)
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:  # pragma: no cover
+        if self._seed is not None:
+            # Restore random states
+            random.setstate(self._random_state)
+            np.random.set_state(self._np_random_state)
+            torch.set_rng_state(self._torch_rng_state)
+            torch.cuda.set_rng_state_all(self._torch_cuda_rng_state_all)
